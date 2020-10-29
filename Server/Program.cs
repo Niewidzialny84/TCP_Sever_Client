616e6ecbdfa3c9ade7d97b6564d8ca2ab504861e
Niewidzialny84 <niewidzialny84@vp.pl>
2020-10-29 18:21:20 +0100

    Added server echo class

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace Server
@@ -9,6 +10,8 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            TCPLib.ServerSyncEcho serverSyncEcho = new TCPLib.ServerSyncEcho(IPAddress.Parse("127.0.0.1"), 40000);
+            serverSyncEcho.Start();
         }
     }
 }
