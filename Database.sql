75d94985a3a2fe5f42384f89afd422cc36774e83
Niewidzialny84 <niewidzialny84@vp.pl>
2020-10-30 12:12:03 +0100

    Added Database.sql file with all needed informations

---
+++ Database.sql
@@ -0,0 +1,30 @@
+--CREATE DATABASE Communication;
+
+--USE Communication;
+
+/*CREATE TABLE Users (
+	UserID int NOT NULL,
+	Login varchar(255),
+	Password varchar(255),
+	PRIMARY KEY (UserID)
+);*/
+
+--INSERT INTO Users (UserID,Login,Password) VALUES ( 1, 'admin', 'admin');
+--INSERT INTO Users (UserID,Login,Password) VALUES ( 2, 'user5', 'qwerty');
+--INSERT INTO Users (UserID,Login,Password) VALUES ( 3, 'xx_GoodPlayer_xx', 'hello');
+
+--SELECT * From Users;
+
+/*CREATE TABLE Responses (
+	ResponseID int NOT NULL,
+	Input varchar(255),
+	Output varchar(255),
+	PRIMARY KEY (ResponseID)
+);*/
+
+--INSERT INTO Responses(ResponseID,Input,Output) VALUES(1,'hi','hello');
+--INSERT INTO Responses(ResponseID,Input,Output) VALUES(2,'hello','welcome here');
+--INSERT INTO Responses(ResponseID,Input,Output) VALUES(3,'bye','goodbye.');
+--INSERT INTO Responses(ResponseID,Input,Output) VALUES(4,'oof','yes oof indeed');
+
+--SELECT * FROM Responses;
