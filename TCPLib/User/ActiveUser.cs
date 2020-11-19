653982550effb7acb5e95312ddf06598f46e392f
Niewidzialny84 <niewidzialny84@vp.pl>
2020-11-19 14:30:03 +0100

    added activeuser class

---
+++ TCPLib/User/ActiveUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCPLib
+{
+    public class ActiveUser : User
+    {
+        private bool logged;
+
+        public ActiveUser(String username, String password) : base(username,password)
+        {
+            logged = true;
+        }
+
+        /// <summary>
+        /// Get/Set current state of the user
+        /// </summary>
+        public bool Logged { get => logged; set => logged = value; }
+    }
+}
