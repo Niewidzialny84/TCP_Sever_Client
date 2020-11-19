fa7df2117b51218e5040dfebd9d1907c53576614
Niewidzialny84 <niewidzialny84@vp.pl>
2020-11-19 14:32:39 +0100

    added id into active user

--- TCPLib/User/ActiveUser.cs
+++ TCPLib/User/ActiveUser.cs
@@ -8,6 +8,7 @@ namespace TCPLib
     public class ActiveUser : User
     {
         private bool logged;
+        private int id;
 
         public ActiveUser(String username, String password) : base(username,password)
         {
@@ -18,5 +19,10 @@ namespace TCPLib
         /// Get/Set current state of the user
         /// </summary>
         public bool Logged { get => logged; set => logged = value; }
+
+        /// <summary>
+        /// Get/Set session id of the active user
+        /// </summary>
+        public int Id { get => id; set => id = value; }
     }
 }
