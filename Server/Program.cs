2679e568f83fb9b1c5d1e15f9c0281be6363a50b
Niewidzialny84 <niewidzialny84@vp.pl>
2020-11-19 15:42:17 +0100

    async using tap

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            TCPLib.Server server = new TCPLib.Server(IPAddress.Parse("127.0.0.1"), 40000);
-            server.Start();
+            //TCPLib.Server server = new TCPLib.Server(IPAddress.Parse("127.0.0.1"), 40000);
+            TCPLib.AsyncComServer server = new TCPLib.AsyncComServer(IPAddress.Parse("127.0.0.1"), 40000);
+            await server.Start();
         }
     }
 }
---
+++ TCPLib/AsyncServer/AsyncComServer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPLib
+{
+    public class AsyncComServer : AsyncAbstractServer
+    {
+        public AsyncComServer(IPAddress ipAddress,int port) : base(ipAddress,port)
+        {
+            Server = new TcpListener(ipAddress, port);
+        }
+
+        public override async Task Start()
+        {
+            Server.Start();
+
+            while(true)
+            {
+                TcpClient client = await Server.AcceptTcpClientAsync();
+
+                byte[] buffer = new byte[Buffer_size];
+                await client.GetStream().ReadAsync(buffer, 0, buffer.Length).ContinueWith(
+                    async(t) =>
+                    {
+                        int i = t.Result;
+                        while(true)
+                        {
+                            await client.GetStream().WriteAsync(buffer,0,i);
+                            i = await client.GetStream().ReadAsync(buffer, 0, buffer.Length);
+                        }
+                    }
+                );
+            }
+        }
+    }
+}
