9df8a0e396f779b7a9d28ed8b86aefa75f51cf83
96peterharris <96peterharris@gmail.com>
2020-11-19 15:06:22 +0100

    Adding functions to manage users

--- TCPLib/User/User.cs
+++ TCPLib/User/User.cs
@@ -19,6 +19,8 @@ namespace TCPLib
             this.password = password;
         }
 
+        public User() { }
+
         /// <summary>
         /// Login getter and setter
         /// </summary>
--- TCPLib/User/UserContainer.cs
+++ TCPLib/User/UserContainer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,24 @@ namespace TCPLib
         public void Add(User user)
         {
             users.Add(user);
+        }
+
+        public void Remove(String login)
+        {
+            if(FindUser(login) == true)
+            {
+                User tmp = new User();
+
+                foreach (User u in users)
+                {
+                    if(u.Login.Equals(login))
+                    {
+                        tmp = u;
+                    }
+                }
+
+                users.Remove(tmp);
+            }
         }
 
         public bool CheckCredentials(String login,String password)
@@ -72,6 +91,88 @@ namespace TCPLib
             {
                 System.Console.Write(e.Message);
             }
+        }
+
+        private void AddUserToDB(String login, String password)
+        {
+           if(FindUser(login) == false)
+           {
+                User user = new User(login, password);
+                int userID = users.Count + 1;
+
+                Add(user);
+              
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "(local)";
+                builder.InitialCatalog = "Communication";
+                builder.IntegratedSecurity = true;
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                    {
+                        String request = "INSERT INTO Users(UserID, Login, Password) VALUES(@id, @login, @password)";               
+                        using (SqlCommand command = new SqlCommand(request, connection))
+                        {
+                            connection.Open();
+                            command.Parameters.Add("@id", SqlDbType.Int).Value = userID;
+                            command.Parameters.Add("@login", SqlDbType.VarChar, 255).Value = login;
+                            command.Parameters.Add("@password", SqlDbType.VarChar, 255).Value = password;
+                            command.CommandType = CommandType.Text;
+                            command.ExecuteNonQuery();
+                            connection.Close();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.Write(e.Message);
+                }
+            }
+        }
+
+        private void RemoveUserFromDB(String login)
+        {
+            if (FindUser(login) == true)
+            {
+                        
+                Remove(login);
+
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "(local)";
+                builder.InitialCatalog = "Communication";
+                builder.IntegratedSecurity = true;
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                    {
+                        String request = "DELETE FROM Users WHERE login= @login";
+                        using (SqlCommand command = new SqlCommand(request, connection))
+                        {
+                            connection.Open();
+                            command.Parameters.Add("@login", SqlDbType.VarChar, 255).Value = login;
+                            command.CommandType = CommandType.Text;
+                            command.ExecuteNonQuery();
+                            connection.Close();
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.Write(e.Message);
+                }
+            }
+        }
+
+        private Boolean FindUser(String login)
+        {
+            foreach(User u in users)
+            {
+                if(u.Login.Equals(login))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
