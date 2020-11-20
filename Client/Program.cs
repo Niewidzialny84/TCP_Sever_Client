7546687e2c05a55350e0bad324718f9c15566066
CatOnXTC <karolsienkiewicz03@gmail.com>
2020-11-20 19:08:08 +0100

    Update Client Program

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,8 +6,15 @@ using System.Text;
 
 namespace Client
 {
+    /// <summary>
+    /// This program is used to start the client.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Creates and starts a client. Connects to IP 127.0.0.1:40000.
+        /// </summary>
+        /// <param name="args">Unused</param>
         static void Main(string[] args)
         {
             TCPLib.Client client = new TCPLib.Client(IPAddress.Parse("127.0.0.1"), 40000);
