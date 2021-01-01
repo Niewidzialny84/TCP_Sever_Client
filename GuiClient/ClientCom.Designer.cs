c4bb31b9e7748809593d5d0889c0f613dad781eb
sulkowski99 <sulkowski99@gmail.com>
2021-01-01 20:06:02 +0100

    Predefined Commands Commit

--- GuiClient/ClientCom.Designer.cs
+++ GuiClient/ClientCom.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.predBox = new System.Windows.Forms.ComboBox();
             this.msgbox = new System.Windows.Forms.TextBox();
             this.sendButton = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
@@ -40,26 +45,91 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.passBox);
+            this.groupBox1.Controls.Add(this.userBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.predBox);
             this.groupBox1.Controls.Add(this.msgbox);
             this.groupBox1.Controls.Add(this.sendButton);
             this.groupBox1.Location = new System.Drawing.Point(12, 45);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(392, 198);
+            this.groupBox1.Size = new System.Drawing.Size(392, 201);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Message";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(253, 138);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Admin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // passBox
+            // 
+            this.passBox.Enabled = false;
+            this.passBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.passBox.Location = new System.Drawing.Point(136, 136);
+            this.passBox.Name = "passBox";
+            this.passBox.Size = new System.Drawing.Size(100, 20);
+            this.passBox.TabIndex = 5;
+            this.passBox.Text = "password/max";
+            this.passBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // userBox
+            // 
+            this.userBox.Enabled = false;
+            this.userBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.userBox.Location = new System.Drawing.Point(21, 136);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(100, 20);
+            this.userBox.TabIndex = 4;
+            this.userBox.Text = "username/min";
+            this.userBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.userBox.TextChanged += new System.EventHandler(this.userBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Predefined commands";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // predBox
+            // 
+            this.predBox.FormattingEnabled = true;
+            this.predBox.Items.AddRange(new object[] {
+            "Show all users",
+            "Delete user",
+            "Add user",
+            "Modify user",
+            "Random number"});
+            this.predBox.Location = new System.Drawing.Point(253, 109);
+            this.predBox.Name = "predBox";
+            this.predBox.Size = new System.Drawing.Size(121, 21);
+            this.predBox.TabIndex = 2;
+            this.predBox.SelectedIndexChanged += new System.EventHandler(this.predBox_SelectedIndexChanged);
             // 
             // msgbox
             // 
             this.msgbox.Location = new System.Drawing.Point(6, 19);
             this.msgbox.Multiline = true;
             this.msgbox.Name = "msgbox";
-            this.msgbox.Size = new System.Drawing.Size(380, 135);
+            this.msgbox.Size = new System.Drawing.Size(380, 84);
             this.msgbox.TabIndex = 1;
             // 
             // sendButton
             // 
-            this.sendButton.Location = new System.Drawing.Point(155, 160);
+            this.sendButton.Location = new System.Drawing.Point(146, 172);
             this.sendButton.Name = "sendButton";
             this.sendButton.Size = new System.Drawing.Size(75, 23);
             this.sendButton.TabIndex = 0;
@@ -70,9 +140,9 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.responseLabel);
-            this.groupBox2.Location = new System.Drawing.Point(12, 249);
+            this.groupBox2.Location = new System.Drawing.Point(12, 276);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(392, 100);
+            this.groupBox2.Size = new System.Drawing.Size(392, 73);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Received";
@@ -81,15 +151,15 @@
             // responseLabel
             // 
             this.responseLabel.AutoSize = true;
-            this.responseLabel.Location = new System.Drawing.Point(161, 32);
+            this.responseLabel.Location = new System.Drawing.Point(18, 25);
             this.responseLabel.Name = "responseLabel";
             this.responseLabel.Size = new System.Drawing.Size(0, 13);
             this.responseLabel.TabIndex = 0;
             // 
-            // button1
+            // logoutButton
             // 
             this.logoutButton.Location = new System.Drawing.Point(329, 12);
-            this.logoutButton.Name = "button1";
+            this.logoutButton.Name = "logoutButton";
             this.logoutButton.Size = new System.Drawing.Size(75, 23);
             this.logoutButton.TabIndex = 2;
             this.logoutButton.Text = "Logout";
@@ -101,8 +171,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(416, 361);
-            this.Controls.Add(this.logoutButton);
             this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.logoutButton);
             this.Controls.Add(this.groupBox1);
             this.Name = "ClientCom";
             this.Text = "ClientCom";
@@ -122,5 +192,10 @@
         private System.Windows.Forms.Label responseLabel;
         private System.Windows.Forms.TextBox msgbox;
         private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox predBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- GuiClient/ClientCom.cs
+++ GuiClient/ClientCom.cs
@@ -22,7 +22,96 @@ namespace GuiClient
 
         private void sendButton_Click(object sender, EventArgs e)
         {
-            this.responseLabel.Text = client.Communicate(msgbox.Text);
+            /*
+            if(this.predBox.Text=="")
+            {
+                this.responseLabel.Text = client.Communicate(msgbox.Text);
+            }
+            else
+
+            Show all users
+            Delete user
+            Add user
+            Modify user
+            Random number
+
+            {
+                 if(this.predBox.Text
+            }*/
+            string msg = "";
+            switch (this.predBox.Text)
+            {
+                case "Show all users":
+                    
+                    this.responseLabel.Text = client.Communicate("getall");
+                    break;
+                case "Delete user":
+                    
+                    if (userBox.Text.Length != 0)
+                    {
+                        msg = "userdel";
+                        msg = msg + " " + userBox.Text;
+                        this.responseLabel.Text = client.Communicate(msg);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Username");
+                    }
+                    break;
+
+                case "Add user":
+                    msg = "useradd";      
+                    if(userBox.Text.Length!=0&&passBox.Text.Length!=0)
+                    {   
+                        if(this.checkBox1.Checked)
+                        {
+                            msg = msg + " " + userBox.Text + " " + passBox.Text + " True";
+                        }
+                        else
+                        {
+                            msg = msg + " " + userBox.Text +" "+ passBox.Text + " False";
+                        }
+
+                       
+                        this.responseLabel.Text = client.Communicate(msg);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Username or Password");
+                    }
+                    break;
+
+                case "Modify user":
+                    msg = "usermod";
+                    if (userBox.Text.Length != 0 && passBox.Text.Length != 0)
+                    {
+                        msg = msg + " " + userBox.Text + " " + passBox.Text;
+                        this.responseLabel.Text = client.Communicate(msg);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Username or Password");
+                    } 
+                    break;
+
+                case "Random number":
+                    msg = "random";
+                   
+                    if (userBox.Text.Length != 0 && passBox.Text.Length != 0)
+                    {  
+                        msg = msg + " " + userBox.Text + " " + passBox.Text;
+                        this.responseLabel.Text = client.Communicate(msg);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid values");
+                    }
+                    break;
+
+                default:
+                    this.responseLabel.Text = client.Communicate(msgbox.Text);
+                    break;
+            }
         }
 
         private void groupBox2_Enter(object sender, EventArgs e)
@@ -34,6 +123,68 @@ namespace GuiClient
         {
             Application.Restart();
             
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void predBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (this.predBox.Text)
+            {
+                case "Show all users":    
+                    this.userBox.Enabled = false;
+                    this.passBox.Enabled = false;
+                    this.checkBox1.Enabled = false;
+                    break;
+
+                case "Delete user":
+                    this.userBox.Enabled = true;
+                    this.passBox.Enabled = false;
+                    this.checkBox1.Enabled = false;
+                    break;
+
+                case "Add user":
+                    this.userBox.Enabled = true;
+                    this.passBox.Enabled = true;
+                    this.checkBox1.Enabled = true;
+                    break;
+
+                case "Modify user":
+                    this.userBox.Enabled = true;
+                    this.passBox.Enabled = true;
+                    this.checkBox1.Enabled = false;                
+                    break;
+
+                case "Random number":
+                    this.userBox.Enabled = true;
+                    this.passBox.Enabled = true;
+                    this.checkBox1.Enabled = false;
+                    break;
+
+                default:
+                    this.userBox.Enabled = false;
+                    this.passBox.Enabled = false;
+                    this.checkBox1.Enabled = false;
+                    break;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userBox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
